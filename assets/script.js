ef9ad2af85bf9a291caaa2e14e1e01e804fe528a
Mathew Harvey <mathewharvey@gmail.com>
2020-09-24 20:17:27 +0800

    updated clock

--- assets/script.js
+++ assets/script.js
@@ -1,73 +1,69 @@
 var APIKey = "07a20a4e47e01a7a29d322ba253e32a7";
-var city = ""
+var city = "";
 
+var currentTime = function () {
+    document.querySelector("#currentTime").innerHTML = moment().format('h:mm:ss a');
+}
+currentTime();
+setInterval(currentTime, 1000);
 
-$("#add-city").on("click",function(event) {
+$("#add-city").on("click", function (event) {
     event.preventDefault();
 
-    let cityInput = $("#city-input").val()
-    city = cityInput
+    let cityInput = $("#city-input").val();
+    city = cityInput;
 
-
-
-
-
-var weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
-$.ajax({
-    url: weatherQueryURL,
-    method: "GET"
-}).then(function (response) {
-
-    var lat = response.coord.lat
-    var long = response.coord.lon
-    var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIKey
-
+    var weatherQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
     $.ajax({
-        url: uvQueryURL,
+        url: weatherQueryURL,
         method: "GET"
     }).then(function (response) {
 
-        var UVDiv = $("<div id='UV'>")
-        var UV = "UV Index: " + response.value
-        var pSix = $("<p>").text(UV)
-        UVDiv.append(pSix)
-        $("#current-weather").append(UVDiv)
+        var lat = response.coord.lat;
+        var long = response.coord.lon;
+        var uvQueryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=" + APIKey;
 
-        console.log(UV)
-    });
+        $.ajax({
+            url: uvQueryURL,
+            method: "GET"
+        }).then(function (response) {
 
-    var cityNameDiv = $("<div id='cityName'>")
-    var timeDateDiv = $("<div id='timeDate'>")
-    var tempCelciusDiv = $("<div id='tempCelcius'>")
-    var humidityDiv = $("<div id='humidity'>")
-    var windSpeedDiv = $("<div id='windSpeed'>")
+            var UVDiv = $("<div id='UV'>");
+            var UV = "UV Index: " + response.value;
+            var pSix = $("<p>").text(UV);
+            UVDiv.append(pSix);
+            $("#current-weather").append(UVDiv);   
+        });
 
+        var cityNameDiv = $("<div id='cityName'>");
+        var timeDateDiv = $("<div id='timeDate'>");
+        var tempCelciusDiv = $("<div id='tempCelcius'>");
+        var humidityDiv = $("<div id='humidity'>");
+        var windSpeedDiv = $("<div id='windSpeed'>");
 
+        var cityName = response.name;
+        var Date = moment().format('MMMM Do YYYY');
+        var tempCelcius = "Temperature: " + ((response.main.temp - 273.15).toFixed(0)) + " degrees celsius";
+        var humidity = "Humidity: " + (response.main.humidity) + " %";
+        var windSpeed = "Wind Speed: " + ((response.wind.speed * 3.6).toFixed(1)) + " km/h";
 
-    var cityName = response.name
-    var timeDate = moment().format('MMMM Do YYYY, h:mm:ss a')
-    var tempCelcius = "Temperature: " + ((response.main.temp - 273.15).toFixed(0))
-    var humidity = "Humidity: " + (response.main.humidity)
-    var windSpeed = "Wind Speed: " + (response.wind.speed)
+        var pOne = $("<p>").text(cityName);
+        var pTwo = $("<p>").text(Date);
+        var pThree = $("<p>").text(tempCelcius);
+        var pFour = $("<p>").text(humidity);
+        var pFive = $("<p>").text(windSpeed);
 
+        cityNameDiv.append(pOne);
+        timeDateDiv.append(pTwo);
+        tempCelciusDiv.append(pThree);
+        humidityDiv.append(pFour);
+        windSpeedDiv.append(pFive);
 
-    var pOne = $("<p>").text(cityName)
-    var pTwo = $("<p>").text(timeDate)
-    var pThree = $("<p>").text(tempCelcius)
-    var pFour = $("<p>").text(humidity)
-    var pFive = $("<p>").text(windSpeed)
+        $("#current-weather").append(cityNameDiv);
+        $("#current-weather").append(timeDateDiv);
+        $("#current-weather").append(tempCelciusDiv);
+        $("#current-weather").append(humidityDiv);
+        $("#current-weather").append(windSpeedDiv);
 
-    cityNameDiv.append(pOne)
-    timeDateDiv.append(pTwo)
-    tempCelciusDiv.append(pThree)
-    humidityDiv.append(pFour)
-    windSpeedDiv.append(pFive)
-
-    $("#current-weather").append(cityNameDiv)
-    $("#current-weather").append(timeDateDiv)
-    $("#current-weather").append(tempCelciusDiv)
-    $("#current-weather").append(humidityDiv)
-    $("#current-weather").append(windSpeedDiv)
-
-})
+    })
 })
